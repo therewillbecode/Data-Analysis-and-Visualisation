d958a66dc8f58baf220fa002caea37d4f888e3b1
TomChambrier <tomw08@gmail.com>
2015-10-13 17:49:24 +0100

    validation.py updates

--- tests/Test_Validation.py
+++ tests/Test_Validation.py
@@ -8,16 +8,16 @@ import validation
 def random_choice(testargs):
     return r.choice(testargs)
 
-TestArgs = [r.randrange(-10000000, 10000000), r.random()*100, (r.random()*-100), r.randrange(700),
+TestArgs = [r.randrange(-10000000, 10000000), r.random()*100, (r.random()*-100),
             'B', 'b', 'dobhnio dobgjudo', 'glrgnkrglknd\gd/rlk332fkbhj', 'UK', '$500', '-$500', 'joe\'s fast run usa']
 
-series = []
-[series.append(random_choice(TestArgs)) for x in range(0, 90)]
-
-print(series)
+rand_list = []
+[rand_list.append(random_choice(TestArgs)) for x in range(0, 90)]
+pseries = pd.series(rand_list)
+print(rand_list)
 
 class Testreadcsv(unittest.TestCase):
         def test_readcsv(self):
-            x = series
+            x = rand_list
             self.assertEqual(all(x) > 1, True)
 
--- validation.py
+++ validation.py
@@ -1,5 +1,27 @@
 __author__ = 'Tom'
 #import index
 import pandas as pd
+import random as r
 
 # takes pandas series and cleans
+
+
+def random_choice(testargs):
+    return r.choice(testargs)
+
+TestArgs = [r.randrange(-10000000, 10000000), r.random()*100, (r.random()*-100),
+            'B', 'b', 'dobhnio dobgjudo', 'glrgnkrglknd\gd/rlk332fkbhj', 'UK', '$500', '-$500', 'joe\'s fast run usa']
+
+rand_list = []
+[rand_list.append(random_choice(TestArgs)) for x in range(0, 90)]
+
+print(rand_list)
+pseries = pd.Series(rand_list)
+print(pseries)
+
+def remove_neg_int(series):
+  f=  [i* for i in series]
+  print(f)
+
+
+remove_neg_int(pseries)
