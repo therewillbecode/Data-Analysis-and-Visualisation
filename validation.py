989e1846f76b88853355b7326b42b725e8afbc20
TomChambrier <tomw08@gmail.com>
2015-10-13 23:24:38 +0100

    altered remove_neg_int function

--- validation.py
+++ validation.py
@@ -9,7 +9,7 @@ import random as r
 def random_choice(testargs):
     return r.choice(testargs)
 
-TestArgs = [r.randrange(-10000000, 10000000), r.random()*100, (r.random()*-100),
+TestArgs = [r.randrange(-10000000, 10000000), r.randrange(0,100000), (r.random()*-100),
             'B', 'b', 'dobhnio dobgjudo', 'glrgnkrglknd\gd/rlk332fkbhj', 'UK', '$500', '-$500', 'joe\'s fast run usa']
 
 rand_list = []
@@ -20,13 +20,17 @@ pseries = pd.Series(rand_list)
 print(pseries)
 
 # use apply function on df/series to execute the below!!!!
-def remove_neg_int(series)
-   # use pd.dataframe.mask to return only elements of a given type
-  f=  [i* for i in series]
-  print(f)
+def remove_neg_int(x):   # only works on intergers not float/decimal
+    bool = False
+    if type(x) == "float":
+        x = round(int(x))
+    if str.isdigit(str(x)):
+        bool = int(x) > 0
+    return bool
 
+h = pseries.apply(remove_neg_int)
+print(h)
 
-remove_neg_int(pseries)
 
 
 #checkout apply map, filter and map for pandas
