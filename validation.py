86295e07ddebebd8cb6acfe602cbc3e440351cf3
TomChambrier <tomw08@gmail.com>
2015-10-13 19:57:30 +0100

    added some notes

--- validation.py
+++ validation.py
@@ -19,9 +19,14 @@ print(rand_list)
 pseries = pd.Series(rand_list)
 print(pseries)
 
-def remove_neg_int(series):
+# use apply function on df/series to execute the below!!!!
+def remove_neg_int(series)
+   # use pd.dataframe.mask to return only elements of a given type
   f=  [i* for i in series]
   print(f)
 
 
 remove_neg_int(pseries)
+
+
+#checkout apply map, filter and map for pandas
