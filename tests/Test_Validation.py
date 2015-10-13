739a3a26210a9e93acf4a41cb7ec2b6dbc3b2f88
TomChambrier <tomw08@gmail.com>
2015-10-13 13:27:30 +0100

    edited valid test

--- tests/Test_Validation.py
+++ tests/Test_Validation.py
@@ -5,18 +5,19 @@ import random as r
 import validation
 
 
-TestArgs = [r.randrange(-10000000, 10000000), r.random()*100, (r.random()*-100),
-            'B', 'b', 'dobhnio dobgjudo','glrgnkrglknd\gd/rlk332fkbhj', 'UK', '$500', '-$500','joe\'s fast run usa']
+def random_choice(testargs):
+    return r.choice(testargs)
 
-sample = r.choice(TestArgs)
+TestArgs = [r.randrange(-10000000, 10000000), r.random()*100, (r.random()*-100), r.randrange(700),
+            'B', 'b', 'dobhnio dobgjudo', 'glrgnkrglknd\gd/rlk332fkbhj', 'UK', '$500', '-$500', 'joe\'s fast run usa']
 
-series = pd.Series
+series = []
+[series.append(random_choice(TestArgs)) for x in range(0, 90)]
 
-[series.append(sample) for x in range(0, r.randrange(0, 90))]
 print(series)
 
 class Testreadcsv(unittest.TestCase):
         def test_readcsv(self):
-            df = sample
-            self.assertEqual(sample > 1, True)
+            x = series
+            self.assertEqual(all(x) > 1, True)
 
