fbdf1abc8fc42f94509312f386db44ffbb35f3e3
TomChambrier <tomw08@gmail.com>
2015-10-17 12:33:20 +0100

    removede a test

--- tests/Data_Cleaning_Tests/Test_Check_Index_Map.py
+++ tests/Data_Cleaning_Tests/Test_Check_Index_Map.py
@@ -20,14 +20,11 @@ class test_(unittest.TestCase):
             self.assertEquals(len(remove(df, 2, neg)), 3)
 
 class test_handles_unexpected_input(unittest.TestCase):
-        def test_should_handle_empty_dataframe(self):
-            print('')
-
         def test_raise_TypeError_for_non_dataframe_arg1(self):
-           self.assertRaises(TypeError, (remove('qwerty', 3, neg)))
+            self.assertRaises(TypeError, (remove('qwerty', 3, neg)))
 
         def test_raise_TypeError_for_non__arg2(self):
-           self.assertRaises(TypeError, (remove(df, [2, 2], neg)))
+            self.assertRaises(TypeError, (remove(df, [2, 2], neg)))
 
         def test_raise_IndexError_for_out_of_range(self):
-           self.assertRaises(IndexError, (remove(df, 90, neg)))
+            self.assertRaises(IndexError, (remove(df, 90, neg)))
