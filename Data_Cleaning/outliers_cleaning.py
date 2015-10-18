9cafa7d2964e82817920125e4f7b698c9271fe18
TomChambrier <tomw08@gmail.com>
2015-10-18 13:45:38 +0100

    testing modified for outliers

--- Data_Cleaning/outliers_cleaning.py
+++ Data_Cleaning/outliers_cleaning.py
@@ -13,19 +13,19 @@ def num(s):
 
 
 # returns a dataframe with outliers for std dev removed
-def remove_outliers(df, sd):
+def remove_outliers(df, sd, col):
     sd = num(sd)
     try:
-         df = df[np.abs(df.Data-df.Data.mean()) <= (sd*df.Data.std())] #keep only the ones that are within +sd to -sd standard deviations in the column 'Data'.
-         df = df[~(np.abs(df.Data-df.Data.mean()) > (sd*df.Data.std()))] #or if you prefer the other way around
+         df = df[np.abs(df[col]-df[col].mean()) <= (sd*df[col].std())] #keep only the ones that are within +sd to -sd standard deviations in the column 'Data'.
          return df
-    except (TypeError, AttributeError):
-        print('arg1 must be a DataFrame')
+    except (TypeError):
+         print(TypeError('arg1 must be a DataFrame'))
+ #   except (AttributeError):
+  #       print('AttributeError')
 
 
-
-def rolling_std(pd_Series):
-    return pd.rolling_std(pd_Series, 25, min_periods=1)
+#def rolling_std(pd_Series):
+#    return pd.rolling_std(pd_Series, 25, min_periods=1)
 
 
 df = pd.DataFrame({'Data':np.random.normal(size=200)})  #example dataset of normally distributed data.
--- tests/Data_Cleaning_Tests/Test_Outliers_Cleaning.py
+++ tests/Data_Cleaning_Tests/Test_Outliers_Cleaning.py
@@ -1,32 +1,35 @@
 __author__ = 'Tom'
-__author__ = 'Tom'
 import unittest
 import pandas as pd
 import numpy as np
 from Data_Cleaning import outliers_cleaning as v
-df=pd.DataFrame({'Data':np.random.normal(size=200)})  #example dataset of normally distributed data.
 
-print(df.__class__.__name__)
-print('g')
+
+df=pd.DataFrame({'Data':np.random.normal(size=200)})  #example dataset of normally distributed data.
 
 
 class test_num_handles_unexpected_input(unittest.TestCase):
+
         def test_num_raises_valueerror(self):
             self.assertRaises(TypeError, v.num('qwerty'))
 
 
 class test_remove_outliers_handles_unexpected_input(unittest.TestCase):
+
         def test_should_raise_attributeerror_for_non_dataframe(self):
-            self.assertRaises(AttributeError, v.remove_outliers('g', 3))
+            self.assertRaises(AttributeError, v.remove_outliers('g', 3,'Data'))
 
         def test_should_accept_digit_string_for_arg2(self):
-            self.assertEqual(v.remove_outliers(df, '4').__class__.__name__, 'DataFrame')
+            self.assertEqual(v.remove_outliers(df, '4','Data').__class__.__name__, 'DataFrame')
 
         def test_should_accept_int_for_arg2(self):
-            self.assertEqual(v.remove_outliers(df, 4).__class__.__name__, 'DataFrame')
+            self.assertEqual(v.remove_outliers(df, 4,'Data').__class__.__name__, 'DataFrame')
 
 
 class test_remove_outliers_handles_boolean_input(unittest.TestCase):
-        def test_can_handle_boolean_input(self):
-            self.assertEqual(v.remove_outliers(df, 4).__class__.__name__, 'DataFrame')
+        def test_delete_row_containing_300(self):
+            df2 = pd.DataFrame({'Data': [1, 2, 3, 4, 9, 300]})   # example dataset with one outlier
+            print('hh')
+            print(v.remove_outliers(df2, 3, 'Data'))
+            self.assertEqual(v.remove_outliers(df2, 2, 'Data').Data.count(), 5)
 
