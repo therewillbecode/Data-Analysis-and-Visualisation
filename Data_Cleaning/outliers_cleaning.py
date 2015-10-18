b30fcb8b4f7592a3e8e67ea31f40d3e7f3f3b554
TomChambrier <tomw08@gmail.com>
2015-10-18 12:09:32 +0100

    testing added for remove outliers

--- Data_Cleaning/outliers_cleaning.py
+++ Data_Cleaning/outliers_cleaning.py
@@ -2,16 +2,33 @@ __author__ = 'Tom'
 import pandas as pd
 import numpy as np
 
-df=pd.DataFrame({'Data':np.random.normal(size=200)})  #example dataset of normally distributed data.
+def num(s):
+    try:
+        return int(s)
+    except ValueError:
+        try:
+            return float(s)
+        except ValueError:
+            print('not a valid int or float')
 
+
+# returns a dataframe with outliers for std dev removed
 def remove_outliers(df, sd):
-    df = df[np.abs(df.Data-df.Data.mean())<=(sd*df.Data.std())] #keep only the ones that are within +3 to -3 standard deviations in the column 'Data'.
-    df = df[~(np.abs(df.Data-df.Data.mean())>(sd*df.Data.std()))] #or if you prefer the other way around
-    return df
+    sd = num(sd)
+    try:
+         df = df[np.abs(df.Data-df.Data.mean()) <= (sd*df.Data.std())] #keep only the ones that are within +sd to -sd standard deviations in the column 'Data'.
+         df = df[~(np.abs(df.Data-df.Data.mean()) > (sd*df.Data.std()))] #or if you prefer the other way around
+         return df
+    except (TypeError, AttributeError):
+        print('arg1 must be a DataFrame')
+
 
 
 def rolling_std(pd_Series):
     return pd.rolling_std(pd_Series, 25, min_periods=1)
 
-print(df)
-print(remove_outliers(df, 3))
+
+df = pd.DataFrame({'Data':np.random.normal(size=200)})  #example dataset of normally distributed data.
+
+#print(df)
+#print(remove_outliers(df, 3))
--- tests/Data_Cleaning_Tests/Test_Outliers_Cleaning.py
+++ tests/Data_Cleaning_Tests/Test_Outliers_Cleaning.py
@@ -1,20 +1,32 @@
 __author__ = 'Tom'
 __author__ = 'Tom'
 import unittest
-
+import pandas as pd
+import numpy as np
 from Data_Cleaning import outliers_cleaning as v
+df=pd.DataFrame({'Data':np.random.normal(size=200)})  #example dataset of normally distributed data.
+
+print(df.__class__.__name__)
+print('g')
 
 
-class test_can_handle_strings(unittest.TestCase):
-        def test_should_return_true_for_negative_int(self):
-            self.assertEqual(v.check_neg_num('-40'), True)
+class test_num_handles_unexpected_input(unittest.TestCase):
+        def test_num_raises_valueerror(self):
+            self.assertRaises(TypeError, v.num('qwerty'))
 
 
-class test_can_handle_boolean_input(unittest.TestCase):
+class test_remove_outliers_handles_unexpected_input(unittest.TestCase):
+        def test_should_raise_attributeerror_for_non_dataframe(self):
+            self.assertRaises(AttributeError, v.remove_outliers('g', 3))
+
+        def test_should_accept_digit_string_for_arg2(self):
+            self.assertEqual(v.remove_outliers(df, '4').__class__.__name__, 'DataFrame')
+
+        def test_should_accept_int_for_arg2(self):
+            self.assertEqual(v.remove_outliers(df, 4).__class__.__name__, 'DataFrame')
+
+
+class test_remove_outliers_handles_boolean_input(unittest.TestCase):
         def test_can_handle_boolean_input(self):
-            self.assertEqual(v.check_neg_num(True), False)
+            self.assertEqual(v.remove_outliers(df, 4).__class__.__name__, 'DataFrame')
 
-
-class test_can_handle_boolean_input(unittest.TestCase):
-        def test_can_handle_boolean_input(self):
-            self.assertEqual(v.check_neg_num(True), False)
