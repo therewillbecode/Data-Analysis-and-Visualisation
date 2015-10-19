f75aeb336b5cdf520d4e6bebddd4827f1bfed9e1
TomChambrier <tomw08@gmail.com>
2015-10-19 16:22:23 +0100

    index_refactored ficed read_csv

--- index_refactored.py
+++ index_refactored.py
@@ -6,18 +6,21 @@ from Analysis import Manipulate_Dfs
 from Data_Cleaning import Time_Series_Cleaning as ts
 
 
-def readcsv(csvfile, encoding, index_col=0, parse_dates='created_at'):
+def readcsv(csvfile, encoding, index_col='country', parse_dates='created_at'):
     try:
         df = pd.read_csv(csvfile, index_col=index_col, encoding=encoding, parse_dates=parse_dates)
-        return df
+        return df.dropna()
     except OSError:
         print('file does not exist')
     except UnicodeDecodeError:
         print('encoding is the wrong codec for file')
 
-# set variable for csv read
+
+# set variables for csv read
 encoding = "ISO-8859-1"
 file = 'raw_data.csv'
+indexCol = 'country'
 
 # call readcsv and assign results to _raw
-df_raw = readcsv(file, encoding=encoding)
+df_raw = readcsv(file, encoding=encoding, index_col=indexCol)
+print(df_raw.columns)
